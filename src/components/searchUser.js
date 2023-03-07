788a65b465b1984dc7a9e7796290b28cabdc6c3d
MarceloMosl <marcelomosl@icloud.com>
2023-03-07 19:49:49 -0300

    Feat: Tela de pagina do usuario com requisição direta do banco de dados (antes do deploy) Marcelo Mosl

--- src/components/searchUser.js
+++ src/components/searchUser.js
@@ -1,15 +1,27 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 export function SearchUser() {
-  const [userPosts, setUsersPosts] = React.useState({});
+  const [userPosts, setUsersPosts] = React.useState([
+    { headline: "", post_url: "" },
+  ]);
 
   useEffect(() => {
-    const promise = axios.get(`localhost:5000/user/${1}`, {
-      headers: { Authorization: `Bearer ${"123456"}` },
+    const promise = axios.get("http://localhost:5000/user/1", {
+      headers: { Authorization: `Bearer ${"1234"}` },
     });
-
-    promise.then(setUsersPosts);
+    promise.then((res) => {
+      setUsersPosts(res.data);
+    });
+    promise.catch((err) => alert(err));
   }, []);
 
-  return <>OLA</>;
+  console.log(userPosts[0].headline);
+
+  return (
+    <>
+      <div>Pagina do Usuario</div>
+      <p>Texto do post: {userPosts[0].headline}</p>
+      <p>URL do post: {userPosts[0].post_url}</p>
+    </>
+  );
 }
