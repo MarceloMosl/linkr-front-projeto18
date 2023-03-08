8c89a6000a8cd85fb4afe5dbbfa4a3b8adffa894
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-03-07 22:20:17 -0300

    feat/trending

---
+++ src/components/TrendingCard/TrendingCard.js
@@ -0,0 +1,12 @@
+import { TrendingContainer } from "./styled"
+import HashTagsList from "../HashTags/HashTags"
+
+export default function TrendingCard(){
+    return(
+        <TrendingContainer>
+            <h1></h1>
+            <div></div>
+            <HashTagsList/>
+        </TrendingContainer>
+    )
+}
---
+++ src/components/TrendingCard/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const TrendingContainer = styled.div`
+  width: 301px;
+  height: 406px;
+  background: #171717;
+  border-radius: 16px;
+
+  h1 {
+    font-family: "Oswald";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 27px;
+    line-height: 40px;
+    margin: 9px 0 12px 16px;
+    /* topo | direita | inferior | esquerda */
+
+    color: #ffffff;
+  }
+
+  div {
+    width: 100%;
+    border: 1px solid #484848;
+    margin-bottom:22px;
+  }
+`;
