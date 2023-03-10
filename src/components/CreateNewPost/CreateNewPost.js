d8330e8c086754f73bd7f6581f83c94b959b4ccd
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-03-10 20:09:10 -0300

    feat create New post

--- src/components/CreateNewPost/CreateNewPost.js
+++ src/components/CreateNewPost/CreateNewPost.js
@@ -1,16 +1,70 @@
-import { CreateNewPostContainer, StyledPostTitle, StyledUserImg } from "./styled";
+import { useState } from "react";
+import { CreateNewPostContainer, Input, PostBox, StyledForm, StyledPostTitle, StyledUserImg, UserImgBox } from "./styled";
+import axios from "axios";
 
 
 export function CreateNewPost() {
+    const [url,setUrl] = useState("")
+    const [postDescription,setPostDescription] = useState("")
+    const [isLoading,setIsLoading] = useState(false)
+
+    async function addNewPost(event){
+        event.preventDefault()
+        setIsLoading(true)
+
+    const body = {
+        url,
+        postDescription
+    }
+
+    try {
+        await axios.post(`${process.env.REACE_APP_API_URL}/newpost`,body)
+        setIsLoading(false)
+        setUrl("")
+        setPostDescription("")
+    } catch (error) {
+        alert(error.response)
+    }
+
+    }
+
     return(
         <CreateNewPostContainer>
+
+            <UserImgBox>
             <StyledUserImg
             src={
                 "https://underbuffed.com/wp-content/uploads/2020/06/Persona-4-Golden-Yosuke-Magician-Thumb.jpg"
             }
             />
+            </UserImgBox>
+
+            <PostBox>
             <StyledPostTitle>What are you going to share today?</StyledPostTitle>
-                
+            <StyledForm onSubmit={addNewPost}>
+                <Input
+                class="input-url"
+                type="url"
+                placeholder="http://..."
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                disabled={isLoading}
+                required
+                />
+                <Input
+                class="InputDescription"
+                type="text"
+                placeholder="Awesome article about #javascript"
+                value={postDescription}
+                onChange={(e) => setUrl(e.target.value)}
+                disabled={isLoading}
+                required
+                />
+                <button type="submit" disabled={isLoading}>
+                    Publicar
+                </button>    
+            </StyledForm>
+            </PostBox>
             
         </CreateNewPostContainer>
     )
--- src/components/CreateNewPost/styled.js
+++ src/components/CreateNewPost/styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const CreateNewPostContainer = styled.div`
+  display: flex;
   width: 611px;
   height: 209px;
   margin-top: 43px;
@@ -15,21 +16,78 @@ export const CreateNewPostContainer = styled.div`
   padding-bottom: 20px;
   padding-right: 21px;
 `;
+
+export const UserImgBox = styled.div``;
+
 export const StyledUserImg = styled.img`
-/* display: flex; */
+  /* display: flex; */
   width: 50px;
   height: 50px;
   border-radius: 26.5px;
-  
+`;
+export const PostBox = styled.div`
+margin-left:18px;
+`;
+export const StyledPostTitle = styled.div`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+
+  color: #707070;
+`;
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    
+    width: 503px;
+    height: 30px;
+
+    background: #efefef;
+    border-radius: 5px;
+    border: none;
+
+    ::placeholder {
+      font-family: "Lato";
+      font-style: normal;
+      font-weight: 300;
+      font-size: 15px;
+      line-height: 18px;
+      color: #949494;
+    }
+    
+  }
+
+  button {
+    width: 112px;
+    height: 31px;
+
+    background: #1877f2;
+    border-radius: 5px;
+    border: none;
+
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 17px;
+
+    color: #ffffff;
+    align-items:flex-end
+  }
 `;
 
-export const StyledPostTitle = styled.div`
-font-family: 'Lato';
-font-style: normal;
-font-weight: 300;
-font-size: 20px;
-line-height: 24px;
 
-color: #707070;
-`
-
+export const Input = styled.input`
+  &.input-url {
+    width: 200px;
+  }
+  
+  &.InputDescription {
+    width: 100px;
+  }
+  `
+;
