447c3b17e945b74e9b7388030a156850d74706d2
C-Macargo <caio.camargos97@gmail.com>
2023-03-22 18:34:57 -0300

    feat, token authentication

--- src/pages/TimelinePage/TimelinePage.js
+++ src/pages/TimelinePage/TimelinePage.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
 import DisplayPost from "../../components/PostContainer/post.js";
 import TrendingCard from "../../components/TrendingCard/TrendingCard.js";
@@ -7,43 +7,46 @@ import { TimelineHeader } from "../../components/TimelineHeader/TimelineHeader.j
 import { CreateNewPost } from "../../components/CreateNewPost/CreateNewPost.js";
 import Header from "../../components/Header/Header.js";
 import UserContext from "../../contexts/UserContext.js";
+import { useNavigate, Link } from "react-router-dom";
 
-/*
-const url = 'https://www.google.com';
-const urlMetadata = require('url-metadata');
-urlMetadata(url).then(metadata => {
-    console.log(metadata)
-}
-)
-*/
 export function Timeline() {
-  const { setHeaderStatus } = useContext(UserContext);
-  setHeaderStatus(true);
+	const token = localStorage.getItem("token");
+	const navigate = useNavigate();
 
-  return (
-    <>
-      <Header />
-      <TimeLineContainer>
-        <TimeLineBox>
-          <PostArea>
-            <TimelineHeader />
-            <CreateNewPost />
-            <DisplayPost></DisplayPost>
-          </PostArea>
-          <TrendingCard />
-        </TimeLineBox>
-      </TimeLineContainer>
-    </>
-  );
+	useEffect(() => {
+		if (!token) {
+			alert("authentication failed, please login");
+			navigate("/");
+		}
+	}, [token]);
+
+	const { setHeaderStatus } = useContext(UserContext);
+	setHeaderStatus(true);
+
+	return (
+		<>
+			<Header />
+			<TimeLineContainer>
+				<TimeLineBox>
+					<PostArea>
+						<TimelineHeader />
+						<CreateNewPost />
+						<DisplayPost></DisplayPost>
+					</PostArea>
+					<TrendingCard />
+				</TimeLineBox>
+			</TimeLineContainer>
+		</>
+	);
 }
 
 const PostArea = styled.div`
-  @media (max-width: 950px) {
-    margin: auto;
-  }
-  @media (max-width: 750px) {
-    margin: auto;
-    width: 100vw;
-  }
-  width: 70%;
+	@media (max-width: 950px) {
+		margin: auto;
+	}
+	@media (max-width: 750px) {
+		margin: auto;
+		width: 100vw;
+	}
+	width: 70%;
 `;
