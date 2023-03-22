bc7957232c047b562ede4173c5c207477b925f7a
MarceloMosl <marcelomosl@icloud.com>
2023-03-21 22:13:15 -0300

    Hotfix: TIMELINE

--- src/components/TimelineHeader/styled.js
+++ src/components/TimelineHeader/styled.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const TimelineHeaderContainer = styled.div`
+  margin-top: 60px;
+  margin-bottom: 20px;
+  padding: 15px;
+  box-sizing: border-box;
   h1 {
     font-family: "Oswald";
     font-style: normal;
